83ce884cd6cbd85193577101d26c0d1760658157
Sofía Errázuriz <sofiaerrazurizm@uc.cl>
2026-02-18 08:41:16 +0100

    Changed determinant so it's exact

--- Proposition2 (linear span check).jl
+++ Proposition2 (linear span check).jl
@@ -3,6 +3,8 @@ using LinearAlgebra
 
 R,x = polynomial_ring(QQ, :x)
 
+# Functions to generate random cameras in P_d,delta
+
 function A(delta)
     if delta == 0
         return [0,0,0]
@@ -28,11 +30,14 @@ function C(d)
 end
 
 function randomPlucker()
+    # Function returns the Plucker coordinates for a random line
     points = rand(-20:20,6)
     return vcat(points[1:3]-points[4:6], cross(points[1:3],points[4:6]))
 end
 
 function lineCurve(d, delta, l)
+    # Function that has the camera parameters d and delta and the number of lines and outputs
+    # the coefficients of the image curves of l random lines through a random camera in P_d,delta
     P = [cayleyTransform(A(delta)), C(d)]
     L = randomPlucker()
     r = [1,0,-x]
@@ -55,7 +60,7 @@ end
 
 function isLinearSpanFull(d, delta, l)
     M = matrix([lineCurve(d, delta, l) for i in 1:l*(2*d+4*delta+3)])
-    if det(M) != 0
+    if LinearAlgebra.det_bareiss(BigInt.(M)) != 0
         return true
     else 
         return false
@@ -64,7 +69,7 @@ end
 
 for d in 0:5
     for delta in 0:5
-        for l in 0:6
+        for l in 1:6
             print(isLinearSpanFull(d,delta,l))
         end
     end
